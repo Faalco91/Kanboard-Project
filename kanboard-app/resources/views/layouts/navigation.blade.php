fa6fcf6a8c7761edaae8153444b9a901e6704de2
celine <celineartemis2@gmail.com>
2025-07-21 01:32:54 +0200

    fix: correction de la route statistics dans la navigation

--- kanboard-app/resources/views/layouts/navigation.blade.php
+++ kanboard-app/resources/views/layouts/navigation.blade.php
@@ -27,7 +27,7 @@
                         {{ __('Projets') }}
                     </x-nav-link>
                     
-                    <x-nav-link :href="route('statistics')" :active="request()->routeIs('statistics')" class="focus-ring">
+                    <x-nav-link href="/statistics" :active="request()->routeIs('statistics')" class="focus-ring">
                         <i class="fas fa-chart-bar mr-2"></i>
                         {{ __('Statistiques') }}
                     </x-nav-link>
