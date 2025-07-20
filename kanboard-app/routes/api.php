b98e875cb71011c88790d9837d62ac7f816b6488
celine <celineartemis2@gmail.com>
2025-07-21 01:23:27 +0200

    feat: ajout de l'API des statistiques avec contrôleur et routes

--- kanboard-app/routes/api.php
+++ kanboard-app/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\API\AuthTokenController;
+use App\Http\Controllers\API\StatisticsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 
@@ -7,4 +8,8 @@ Route::post('/login-token', [AuthTokenController::class, 'login']);
 Route::post('/logout-token', [AuthTokenController::class, 'logout'])->middleware('auth:sanctum');
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('projects', ProjectController::class);
+    
+    // Routes pour les statistiques
+    Route::get('/statistics/dashboard', [StatisticsController::class, 'dashboard']);
+    Route::get('/statistics/projects/{project}', [StatisticsController::class, 'projectStatistics']);
 });
